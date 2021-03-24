96bcf48ce5737b2236543352d9407042c5a36bf7
suhridRai123 <75163860+suhridRai123@users.noreply.github.com>
2021-03-24 10:02:48 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,69 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+var engine, world;
+var box1, pig1;
+var backgroundImg;
+var platform;
+var bird;
+
+function preload() {
+}
+
+function setup(){
+    var canvas = createCanvas(1200,400);
+    engine = Engine.create();
+    world = engine.world;
+
+    
+    ground = new Ground(600,height,1800,20)
+
+    platform = new Ground(600,305,300,20)
+    box1 = new Box(645,130,70,70);
+    box2 = new Box(575,130,70,70);
+
+    box3 = new Box(540,200,70,70);
+    box4 = new Box(680,200,70,70);
+
+
+    box5 = new Box(610,200,70,70);
+
+    bird = new Bird(200,50);
+    
+    Sling = new SlingShot(bird.body, {x:200, y:50})
+}
+
+function draw(){
+    background(0);
+
+    Engine.update(engine);
+    box1.display();
+    box2.display();
+    ground.display();
+    platform.display();
+    box3.display();
+    box4.display();
+
+    box5.display();
+
+    bird.display();
+
+
+    Sling.display();
+}
+
+function mouseDragged(){
+    Matter.Body.setPosition(bird.body,{x:mouseX, y:mouseY})
+}
+
+function mouseReleased(){
+    Sling.fly()
+}
+
+function keyPressed(){
+    if(keyCode === 32){
+        Sling.attach(bird.body);
+    }
+}
